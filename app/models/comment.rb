0b431fd84a4d909825c177bbf082ed15dcc6e808
naidu479 <naidu479@gmail.com>
2016-06-16 07:11:56 -0400

    Added Comment model

---
+++ app/models/comment.rb
@@ -0,0 +1,17 @@
+class Comment < ApplicationRecord
+
+  # Autocode: Relationships
+  belongs_to :user
+  belongs_to :post
+
+  # Autocode: Accept Nested Attributes
+
+  # File Upload
+
+  # Autocode: Validations
+
+  # Autocode: Callbacks
+
+	# Soft Destroy
+
+end
