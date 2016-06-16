2f0a8b071a1f913ead91c640a63aba3129784ccc
naidu479 <naidu479@gmail.com>
2016-06-16 07:13:38 -0400

    Added Relationships for models

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,7 @@ class User < ActiveRecord::Base
         :recoverable, :rememberable, :trackable 
 
   # Autocode: Relationships
+  has_many :posts
   
   has_many :authtokens, dependent: :destroy
   has_many :identities, dependent: :destroy
