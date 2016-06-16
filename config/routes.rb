4fd1bb658d4aae6e615878606377a2d9911518db
naidu479 <naidu479@gmail.com>
2016-06-16 07:12:58 -0400

    Modifying Routes

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,11 @@ Rails.application.routes.draw do
 	# Api End
 
   #Devise Routes For Api
+      devise_scope :user do
+    delete '/logout', :to => 'api/v1/users/sessions#destroy'
+    post '/login', :to => 'api/v1/users/sessions#create'
+    post '/signup', :to => 'api/v1/users/registrations#create'
+  end
 
 
 end
