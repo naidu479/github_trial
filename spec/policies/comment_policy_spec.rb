cdf4729732b46f696f519225de68bc40e721e419
naidu479 <naidu479@gmail.com>
2016-06-16 07:15:21 -0400

    Added Pundit Policy Specs}

--- spec/policies/comment_policy_spec.rb
+++ spec/policies/comment_policy_spec.rb
@@ -6,5 +6,14 @@ describe CommentPolicy do
   let(:comment) { Comment.create }
 
   #add_here
+context "being a/an admin" do
+                          let(:user) {  User.create('roles' => 'admin', 'email' => 'first@first.com', 'password' => 'abcdabcd') }
+                          # Action Specs :: Admin
+it { should permit_action(:destroy) }
+it { should permit_action(:update) }
+it { should permit_action(:show) }
+it { should permit_action(:create) }
+                          # Attribute Specs :: Admin
+                        end
 
 end
