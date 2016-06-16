d8dfd8f508bb294501a97a32da40cd99ce0d484c
naidu479 <naidu479@gmail.com>
2016-06-16 07:13:20 -0400

    Making Email Non Mandatory for Social Logins

--- app/controllers/api/v1/users/registrations_controller.rb
+++ app/controllers/api/v1/users/registrations_controller.rb
@@ -1,21 +1,72 @@
 class Api::V1::Users::RegistrationsController < Api::V1::BaseController
 
   skip_before_filter :authenticate_user_from_token!, :only => [:create]
-  
+
   def create
-    @user = User.new signup_parameters
-    if @user.save
-      @auth_token = Authtoken.create_auth_token(@user.id,request.remote_ip, request.user_agent, signup_parameters[:device_id])
-      render json: {message: "#{@user.email} was succesfully created", auth_token: @auth_token.token}
+    if signup_parameters[:social_auth]
+      auth = signup_parameters[:social_auth].to_h
+      identity = Identity.find_for_oauth(auth)
+      return invalid_signup_attempt if identity.present?
+
+      if identity.present?
+        validated_res = Identity.validate_auth_data(auth['access_token'],auth['provider'])
+        return invalid_access_token if (!validated_res['id'] && !validated_res['user_id'])
+
+        sign_in(identity.user)
+        identity.update_attribute(:accesstoken, auth['access_token'])
+        auth_token = Authtoken.create_auth_token(identity.user.id,request.remote_ip, request.user_agent, signup_parameters[:device_id])
+        render json: {auth_token:auth_token.token}
+      else
+
+        validated_res = Identity.validate_auth_data(auth['access_token'],auth['provider'])
+        return invalid_access_token if (!validated_res['id'] && !validated_res['user_id'])
+
+        user = User.new( email: auth['email'] || "" )
+
+        if user.save(validate: false)
+         identity = Identity.create_identity(auth, user.id)
+         generate_authtoken(user)
+        else
+          send_failure_res(user)
+        end
+      end
+
+    elsif signup_parameters[:email]
+      user = User.new signup_parameters
+
+      if user.save
+        generate_authtoken(user)
+      else
+        send_failure_res(user)
+      end
+
     else
-      render json: {error: @user.errors.full_messages }
+      render status: 400, json: {error: "Please pass correct parameters"}
     end
+
   end
 
   protected
 
+  def generate_authtoken(user)
+    auth_token = Authtoken.create_auth_token(user.id,request.remote_ip, request.user_agent, signup_parameters[:device_id])
+    render json: {message: "#{user.email} was succesfully created", auth_token: auth_token.token}
+  end
+
+  def send_failure_res(user)
+    render status: 400, json: {error: user.errors.full_messages }
+  end
+
+  def invalid_signup_attempt
+    render status: 403, json: {error: "Identity already present"}
+  end
+
+  def invalid_access_token
+    render status: 400, json: {error: "Access Token is Invalid - Oauth Exception"}
+  end
+
   def signup_parameters
-    params.require(:user).permit( :email,:password,:password_confirmation,:device_id)
+    params.require(:user).permit( :email,:password, :password_confirmation, :device_id,:social_auth =>[:provider,:access_token,:uid,:email])
   end
 end
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,17 +1,43 @@
 class User < ActiveRecord::Base
-	
+
 # Include default devise modules. Others available are:
 # :confirmable, :lockable, :timeoutable and :omniauthable
  
  devise :database_authenticatable, :registerable,
-        :recoverable, :rememberable, :trackable, :validatable
+        :recoverable, :rememberable, :trackable 
+
   # Autocode: Relationships
+  
   has_many :authtokens, dependent: :destroy
+  has_many :identities, dependent: :destroy
+
   # Autocode: Validations
 
+  validates_presence_of   :email, if: :email_required?
+  validates_uniqueness_of :email, allow_blank: true, if: :email_changed?
+  validates_format_of     :email, with: Devise.email_regexp, allow_blank: true, if: :email_changed?
+
+  validates_presence_of     :password, if: :password_required?
+  validates_confirmation_of :password, if: :password_required?
+  validates_length_of       :password, within: Devise.password_length, allow_blank: true
+
+  validates_presence_of     :password_confirmation, if: :password_required?
+  validates_confirmation_of :password_confirmation, if: :password_required?
+
+	
   # Autocode: Callback
 
 	# File Upload
 
 	# Soft Destroy
+
+	def password_required?
+    return false if email.blank?
+    !persisted? || !password.nil? || !password_confirmation.nil?
+  end
+
+  def email_required?
+    true
+  end
+
 end
