115a8caeb7b946d112e3e2a87c40e1dfb6c3ff73
naidu479 <naidu479@gmail.com>
2016-06-16 07:11:47 -0400

    Added Post model

---
+++ app/models/post.rb
@@ -0,0 +1,16 @@
+class Post < ApplicationRecord
+
+  # Autocode: Relationships
+  belongs_to :user
+
+  # Autocode: Accept Nested Attributes
+
+  # File Upload
+
+  # Autocode: Validations
+
+  # Autocode: Callbacks
+
+	# Soft Destroy
+
+end
